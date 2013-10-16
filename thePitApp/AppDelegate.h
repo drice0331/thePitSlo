4e67a06156760e8fea4bff2bc4d518676ab2e042
David Rice <drice@Davids-MacBook-Air.local>
2013-10-16 05:07:50 -0400

    Initial Commit

---
+++ thePitApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  thePitApp
+//
+//  Created by David Rice on 10/16/13.
+//  Copyright (c) 2013 David Rice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ thePitApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  thePitApp
+//
+//  Created by David Rice on 10/16/13.
+//  Copyright (c) 2013 David Rice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
