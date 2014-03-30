2b7ba5fe8cdfa56fa3cb53ffd6eebb986ddd59be
David Rice <drice@Davids-MacBook-Air.local>
2014-03-30 15:16:00 -0400

    scrolling for scheduleViewController

---
+++ thePitApp/ScheduleViewContainer.h
@@ -0,0 +1,13 @@
+//
+//  ScheduleViewContainer.h
+//  thePitApp
+//
+//  Created by David Rice on 3/29/14.
+//  Copyright (c) 2014 David Rice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScheduleViewContainer : UIViewController
+
+@end
---
+++ thePitApp/ScrollViewTest.h
@@ -0,0 +1,13 @@
+//
+//  ScrollViewTest.h
+//  thePitApp
+//
+//  Created by David Rice on 3/29/14.
+//  Copyright (c) 2014 David Rice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScrollViewTest : UIViewController
+
+@end
